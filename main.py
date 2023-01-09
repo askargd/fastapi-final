389dddaa22954940cc129032af61a3e3577c4a1f
Askar <your_email@whatever.com>
2023-01-09 19:35:26 +0600

    API get_Fibo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
@@ -12,5 +12,18 @@ def read_root():
 @app.get('/{sum1n}')
 def get_sum1n(n: int):
     n=(n*(n+1)//2)
-    return{"sum": n}
+    return{"result": n}
 
+
+@app.get('/{fibo}')
+def get_fibo(terms: int):
+    n1=0
+    n2=1
+    count=1
+    while count<terms:
+        nth=n1+n2
+        #New values
+        n1=n2
+        n2=nth
+        count += 1
+    return{"result": n1}
